afdbd79b75f262dca64f453e0afee987ad997fe9
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-06-09 14:06:22 +0530

    User component updated

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export const User = () => {
     return (
         <div>
-            <h2>Welcome user</h2>
+            <h2>Welcome user......... </h2>
             <p>This component is created in dev branch</p>
+            <br />
+            <h4>Welcome User updated in master branch after merging  </h4>
         </div>
     )
 }
